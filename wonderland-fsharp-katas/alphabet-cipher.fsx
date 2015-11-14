053991383b488f1a08dfb0f56755dd4a090d70c9
Paul Marfleet <paul.marfleet@gmail.com>
2015-11-14 22:58:11 +0000

    Solved alphabet-cipher kata

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -3,14 +3,42 @@
 type Message = string
 type Keyword = string
 
-let encode (key:Keyword) (message:Message) : Message =
-    "encodeme"
+let substitutionChart : Lazy<Map<char, (char * char)[]>> =
+    lazy
+    (
+        let alphabet = [|'a'..'z'|]
+        let length = Array.length alphabet
+        let getPermutation offset = 
+            let permutation =
+                if (offset < length) then
+                    Array.permute (fun index -> (index + offset) % length) alphabet
+                else
+                    alphabet
+            Array.zip alphabet permutation
+        [ for i in length .. -1 .. 1 -> (alphabet.[length - i], getPermutation i) ] |> Map.ofList
+    )
 
-let decode (key:Keyword) (message:Message) : Message =
-    "decodeme"
+let getLetter (keyLetter:char) (letter:char) (comparer:(char * char) -> char) (selector:(char * char) -> char) : char =         
+    substitutionChart.Value.[keyLetter] 
+        |> Array.find (fun item -> (comparer item) = letter) 
+        |> selector
+
+let encode_decode (key:Keyword) (message:Message) (comparer:(char * char) -> char) (selector:(char * char) -> char) : Message =
+    let encodeLetter i letter = getLetter key.[i % key.Length] letter comparer selector
+    Seq.mapi encodeLetter message |> System.String.Concat
+
+let encode (key:Keyword) (message:Message) : Message = encode_decode key message fst snd
+
+let decode (key:Keyword) (message:Message) : Message = encode_decode key message snd fst
 
 let decipher (cipher:Message) (message:Message) : Keyword =
-    "decypherme"
+    let keyText = Seq.map2 (fun c m -> getLetter m c snd fst) cipher message |> System.String.Concat
+    seq {for i in 1 .. keyText.Length do
+            let chunks = Seq.chunkBySize i keyText
+            let pairedChunks = keyText |> Seq.chunkBySize i |> Seq.pairwise |> List.ofSeq
+            if pairedChunks |> Seq.forall (fun (first, second) -> first = second || first.Length <> second.Length) then
+                yield pairedChunks |> Seq.head |> fst |> System.String.Concat
+    } |> Seq.head
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
